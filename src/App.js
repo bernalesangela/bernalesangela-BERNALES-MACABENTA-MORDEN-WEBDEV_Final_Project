650487fddcf120550ae201ac7ef6616bb2137873
bernalesangela <bernalesangela15@gmail.com>
2025-03-05 22:11:23 +0800

    added create order page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/admin/dashboard";
+import CreateOrder from "./pages/admin/create-order";
 import { LoginPage } from "./pages/login";
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/admin/dashboard" element={<Dashboard />} />
+        <Route path="/admin/create-order" element={<CreateOrder />} />
         <Route path="/login/" element={<LoginPage />} />
       </Routes>
     </Router>
---
+++ src/components/ui/Separator.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Separator = () => {
+  return <div className="w-full h-1 bg-darkerGray rounded-full"></div>;
+};
+
+export default Separator;
