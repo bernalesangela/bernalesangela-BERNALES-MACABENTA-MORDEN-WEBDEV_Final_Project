8c50e8cbe3ac82b74f6642c8db97f385bc49b273
bernalesangela <bernalesangela15@gmail.com>
2025-03-12 23:53:11 +0800

    added about page(unfinished)

--- src/pages/admin/about/index.js
+++ src/pages/admin/about/index.js
@@ -8,6 +8,8 @@ const AboutPage = () => {
         <div className="w-full flex items-center gap-3">
           <h1 className="text-blueSerenity py-5">About</h1>
         </div>
+
+        <div></div>
       </section>
     </Layout>
   );
